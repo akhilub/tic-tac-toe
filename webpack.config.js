dc2343e40fcb9747f978d030bb8e1b39ea60a2f9
Akhil Singh Chauhan <akhilsin@buffalo.edu>
2025-03-10 12:48:08 -0400

    Updated webpack config for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/tic-tac-toe.js",
@@ -6,6 +7,11 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
     module: {
         rules: [
             {
